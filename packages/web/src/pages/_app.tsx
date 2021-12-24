a16686e95eaf315c58d3e46d4e5e193481c5fed3
Christopher Holmok <christopher@holmok.com>
2021-12-24 08:50:07 -0600

    next is now proxying to api

--- packages/web/src/pages/_app.tsx
+++ packages/web/src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
-import { InitClients, Clients, Constants } from '@uniquey.io/common'
+import { InitClients, Clients } from '@uniquey.io/common'
 import 'milligram/dist/milligram.min.css'
 import '../styles/layout.css'
 import '../styles/header.css'
 import '../styles/footer.css'
 
-const clients = InitClients(Constants.apiUrl)
+const clients = InitClients('/api')
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -17,11 +17,11 @@ type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
-export function getClients (): Clients {
+export function getClients(): Clients {
   return clients
 }
 
-export default function MyApp ({ Component, pageProps }: AppPropsWithLayout): ReactNode {
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout): ReactNode {
   const getLayout = Component.getLayout ?? ((page) => page)
   return getLayout(<Component {...pageProps} />)
 }
